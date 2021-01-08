d02b829636c91d48fc9d13e047435399c1c4b588
Naitzirch <christian.ca.martens@gmail.com>
2021-01-08 22:32:21 +0100

    commented line

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
         switch (cmd){
             case "submission":
                 say(channelID, "submission completed!");
+                //say(channelID, Discord.message.url);
                 break;
             case "bind":
                 say(channelID, "RecordsBot will now send submissions to ", );
