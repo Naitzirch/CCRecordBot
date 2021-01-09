1aeff7ad72d0d0545508c64541940c3e1c8b36d7
Naitzirch <christian.ca.martens@gmail.com>
2021-01-09 03:03:30 +0100

    Practically functional

--- index.js
+++ index.js
@@ -15,22 +15,39 @@ bot.on("ready", function (){
 
 
 
-
-bot.on('message', function (user, userID, channelID, message, evt, url) {
+bot.on('message', function (user, userID, channelID, message, evt) {
 
 
     if (message.substring(0, 1) === prefix ){
 
+        let path = "tmp/bind.txt"; //path where channel id is saved
+        let bind;
         let args = message.substring(1).split(" ");
         let cmd = args[0];
         args = args.splice(1);
         switch (cmd){
             case "submission":
-                say(channelID, `https://discord.com/channels/${evt.d.guild_id}/${evt.d.channel_id}/${evt.d.id}`);
-                say(channelID, "submission completed!");
+
+                    if (fs.existsSync(path)) { //check if file exists
+                        fs.readFile(path, 'utf8', function(err, data) {
+                            if (err) throw err;
+                            bind = data; //read channelID from file
+                            say(bind, `https://discord.com/channels/${evt.d.guild_id}/${evt.d.channel_id}/${evt.d.id}`);
+                            say(channelID, "submission completed!");
+                        });
+                    }
+                    else {
+                        say(channelID, "Error: I am not bound to any channel yet! use $bind to define the output channel.")
+                    }
                 break;
             case "bind":
-                say(channelID, "RecordsBot will now send submissions to ", );
+                fs.writeFile(path, channelID, function(err) {
+                    if(err) {
+                        return console.log(err);
+                    }
+                    console.log("The file was saved.");
+                    say(channelID, `RecordBot will now send submissions to <#${channelID}>`);
+                });
                 break;
             case "help":
                 say(channelID, "Help message");
