96f934daa54d5f4ae3a039dcc56377bc49292088
Naitzirch <christian.ca.martens@gmail.com>
2021-01-09 17:52:56 +0100

    Updated queue messages and formatting

--- index.js
+++ index.js
@@ -9,61 +9,78 @@ const bot = new Discord.Client({
     autorun: true
 });
 
-bot.on("ready", function (){
+bot.on("ready", function () {
     console.log("Bot started, logged in as: " + bot.username);
 });
-
 
 
 bot.on('message', function (user, userID, channelID, message, evt) {
 
 
-    if (message.substring(0, 1) === prefix ){
+    if (message.substring(0, 1) === prefix) {
 
         let path = "tmp/bind.txt"; //path where channel id is saved
         let bind;
         let args = message.substring(1).split(" ");
         let cmd = args[0];
         args = args.splice(1);
-        switch (cmd){
+        //console.log(args);
+        switch (cmd) {
             case "submit":
 
-                    if (fs.existsSync(path)) { //check if file exists
-                        fs.readFile(path, 'utf8', function(err, data) {
-                            if (err) throw err;
-                            bind = data; //read channelID from file
-                            //say(bind, ``);
-                            bot.sendMessage({
-                               to: bind,
-                               message: '',
-                               embed: {
-                                   color: 0xfecc52,
-                                   title: `New submission by ${evt.d.author.username}`,
-                                   fields: [{
-                                       name: "Details",
-                                       value:   `IGN: **data**\n` +
-                                                `Game-mode: **GM**\n\n` +
+                if (fs.existsSync(path)) { //check if file exists
+                    fs.readFile(path, 'utf8', function (err, data) {
+                        if (err) throw err;
+                        bind = data; //read channelID from file
+                        //say(bind, ``);
 
-                                                `[**Click here to view their submission**](https://discord.com/channels/${evt.d.guild_id}/${evt.d.channel_id}/${evt.d.id})`
-                                       }
-                                   ]
-                               }
+                        if (args.length > 1) {
+
+                            let IGN = args[0];
+                            let GM = args[1];
+                            args.splice(0,2);
+                            message = args.join(" ");
+                            bot.sendMessage({
+                                to: bind,
+                                message: '',
+                                embed: {
+                                    color: 0xfecc52,
+                                    title: `New submission by ${evt.d.author.username}`,
+                                    fields: [{
+                                        name: "Details",
+                                        value: `IGN: **${IGN}**\n` +
+                                            `Game-mode: **${GM}**\n\n`
+                                    },
+                                        {
+                                            name: "Message:",
+                                            value: message +
+                                                `\n\n[**Click here to view their submission**](https://discord.com/channels/${evt.d.guild_id}/${evt.d.channel_id}/${evt.d.id})`
+                                        }
+                                    ]
+                                }
                             });
                             say(channelID, "submission completed!");
-                        });
-                    }
-                    else {
-                        say(channelID, "Error: I am not bound to any channel yet! use $bind to define the output channel.")
-                    }
+                        } else {
+                            say(channelID, "Not enough arguments. Use `$help` to see proper usage.");
+                        }
+                    });
+                } else {
+                    say(channelID, "Error: I am not bound to any channel yet! use $bind to define the output channel.")
+                }
                 break;
             case "bind": //save a file with the channelID
-                fs.writeFile(path, channelID, function(err) {
-                    if(err) {
-                        return console.log(err);
-                    }
-                    console.log("The file was saved.");
-                    say(channelID, `RecordBot will now send submissions to <#${channelID}>`);
-                });
+                //if () {
+                    fs.writeFile(path, channelID, function (err) {
+                        if (err) {
+                            return console.log(err);
+                        }
+                        console.log("The file was saved.");
+                        say(channelID, `RecordBot will now send submissions to <#${channelID}>`);
+                    });
+                /*}
+                else {
+                    say(channelID, "You do not have permission to use this command.")
+                }*/
                 break;
             case "help":
                 bot.sendMessage({
@@ -75,18 +92,18 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                         //url: 'https://cubecraft.net'
                         fields: [{
                             name: "Commands",
-                            value:  "$help\n" +
-                                    "- Displays all available commands.\n\n" +
+                            value: "$help\n" +
+                                "- Displays this information panel.\n\n" +
 
-                                    "$submit [IGN] [Game-mode]\n" +
-                                    "- Submit your record! \n" +
-                                    "Example of usage:\n" +
-                                    "`$submit rubik_cube_man Parkour`\n\n" +
+                                "$submit [IGN] [Game-mode]\n" +
+                                "- Submit your record! \n" +
+                                "Example of usage:\n" +
+                                "`$submit rubik_cube_man Parkour`\n\n" +
 
-                                    "$bind\n" +
-                                    "Used to bind to submission queue.\n"
+                                "$bind\n" +
+                                "Used to bind to submission queue.\n"
 
-                            }, //You can put [masked links](http://google.com) inside of rich embeds.
+                        }, //You can put [masked links](http://google.com) inside of rich embeds.
                             {
                                 name: "CubeCraft Book of World Records",
                                 value: "[Here](https://www.cubecraft.net/threads/cubecraft-book-of-world-records-revamping.213611/) your records are displayed after they have been reviewed and accepted."
@@ -102,7 +119,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
     }
 });
 
-function say(channelID, message){
+function say(channelID, message) {
     bot.sendMessage({
         to: channelID,
         message: message
