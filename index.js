fcd82e45e40c8becd5f1965ce9dc17e4c55dcd8d
Naitzirch <christian.ca.martens@gmail.com>
2021-01-09 23:05:33 +0100

    Added permission management for bind command

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const FileSync = require('lowdb/adapters/FileSync');
 
 let prefix = "$";
 const bot = new Discord.Client({
-    token: "Nzk3MTY1NTcwOTMxOTQ5NTcy.X_igSg.3mXyoZF-xmUjQf013dIpQPREkIA",
+    token: "Nzk3MTY1NTcwOTMxOTQ5NTcy.X_igSg.3mXyoZF-xmUjQf013dIpQPREkIA", //DONT USE THE SAME TOKEN AFTER DEPLOYMENT
     autorun: true
 });
 
@@ -38,7 +38,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 
                             let IGN = args[0];
                             let GM = args[1];
-                            args.splice(0,2);
+                            args.splice(0, 2);
                             message = args.join(" ");
                             bot.sendMessage({
                                 to: bind,
@@ -59,7 +59,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                                     ]
                                 }
                             });
-                            say(channelID, "submission completed!");
+                            say(channelID, `Your submission will be reviewed! <@${userID}>`);
                         } else {
                             say(channelID, "Not enough arguments. Use `$help` to see proper usage.");
                         }
@@ -69,18 +69,40 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 }
                 break;
             case "bind": //save a file with the channelID
-                //if () {
-                    fs.writeFile(path, channelID, function (err) {
-                        if (err) {
-                            return console.log(err);
+                let roleArr = evt.d.member.roles; //check what roles the user has
+                let rolePath = "tmp/role.txt";
+
+                if (fs.existsSync(rolePath)) { //check if role file exists
+                    fs.readFile(rolePath, 'utf8', function (err, savedRole) {
+                        if (err) throw err;
+
+                        //check if the querying user has the role
+                        if (roleArr.includes(savedRole) || savedRole === "undefined") {
+                            if (typeof args[0] !== 'undefined') { //Check if there is an argument
+
+                                let role = args[0].substring(3, args[0].length - 1);
+
+                                fs.writeFile(rolePath, role, function (err) { //save their role id
+                                    if (err) {
+                                        return console.log(err);
+                                    }
+                                    console.log("The file was saved.");
+                                    say(channelID, `Only <@&${role}> will now be able to use the bind command.`);
+                                });
+                            } else {
+                                fs.writeFile(path, channelID, function (err) {
+                                    if (err) {
+                                        return console.log(err);
+                                    }
+                                    console.log("The file was saved.");
+                                    say(channelID, `RecordBot will now send submissions to <#${channelID}>`);
+                                });
+                            }
+                        } else {
+                            say(channelID, "You do not have permission to use this command.");
                         }
-                        console.log("The file was saved.");
-                        say(channelID, `RecordBot will now send submissions to <#${channelID}>`);
                     });
-                /*}
-                else {
-                    say(channelID, "You do not have permission to use this command.")
-                }*/
+                }
                 break;
             case "help":
                 bot.sendMessage({
@@ -90,27 +112,25 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                         color: 0xfecc52,
                         title: 'RecordBot help',
                         //url: 'https://cubecraft.net'
+                        description:
+                            "[**CubeCraft Book of World Records**](https://www.cubecraft.net/threads/cubecraft-book-of-world-records-revamping.213611/)" +
+                            "\nThis is where your records are displayed after they have been reviewed and accepted.\n\n",
                         fields: [{
                             name: "Commands",
                             value: "$help\n" +
                                 "- Displays this information panel.\n\n" +
 
-                                "$submit [IGN] [Game-mode]\n" +
+                                "$submit [IGN] [Game-mode] [Your message] [evidence]\n" +
                                 "- Submit your record! \n" +
                                 "Example of usage:\n" +
-                                "`$submit rubik_cube_man Parkour`\n\n" +
+                                "`$submit rubik_cube_man Parkour Fastest time Barn 1 5:232s [evidence]`\n\n"
 
-                                "$bind\n" +
-                                "Used to bind to submission queue.\n"
 
-                        }, //You can put [masked links](http://google.com) inside of rich embeds.
-                            {
-                                name: "CubeCraft Book of World Records",
-                                value: "[Here](https://www.cubecraft.net/threads/cubecraft-book-of-world-records-revamping.213611/) your records are displayed after they have been reviewed and accepted."
-                            }
+                        } //You can put [masked links](http://google.com) inside of rich embeds.
+
                         ],
                         footer: {
-                            text: 'Footer'
+                            text: ''
                         }
                     }
                 })
