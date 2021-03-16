13e2286bcbf07a0b68994ff210258773cea3ccde
Naitzirch <christian.ca.martens@gmail.com>
2021-03-16 03:38:35 +0100

    Added json database in which information about the bot is stored as well as user information for a new feature in which you can connect your discord account to your IGN and forums account

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 //Main file
 //Author: Christian Martens
 //Last update: 10/01/2021
-//version 1.1
+//version 1.2
 
 const Discord = require('discord.io');
+
 const low = require('lowdb');
-const fs = require('fs');
 const FileSync = require('lowdb/adapters/FileSync');
 
-let prefix = "$";
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+
+const fs = require('fs');
+
+db.defaults({ botInfo: {}, users: [], help: {} })
+    .write()
+
+
 const bot = new Discord.Client({
-    token: "Nzk3ODc3MjI5MDczNDY1NDY1.X_s3Ew.Xpx9BM8_ZE2IsLnFkle6r0BjQUw", //DONT USE THE SAME TOKEN AFTER DEPLOYMENT
+    token: db.get('botInfo.token').value(), //Read the bot token from the db
     autorun: true
 });
-
 bot.on("ready", function () {
     console.log("Bot started, logged in as: " + bot.username);
 });
+
+let prefix = "$";
 
 
 bot.on('message', function (user, userID, channelID, message, evt) {
@@ -34,75 +43,88 @@ bot.on('message', function (user, userID, channelID, message, evt) {
         switch (cmd) {
             case "submit":
 
-                if (fs.existsSync(path)) { //check if file exists
-                    fs.readFile(path, 'utf8', function (err, data) {
-                        if (err) throw err;
-                        bind = data; //read channelID from file
-                        //say(bind, ``);
+                bind = db.get('botInfo.channelID').value(); //read channelID from db
 
-                        if (args.length > 2) {
+                let IGN
+                let forumLink
+                let GM
 
-                            let IGN = args[0]; //first element is IGN
-                            args = args.splice(1); //remove IGN from array
+                if (db.get('users').find({id: userID}).value()){
+                    IGN = db.get('users').find({id: userID}).value().IGN;
+                    forumLink = db.get('users').find({id: userID}).value().forums;
+                    GM = args[0];
+                    args = args.splice(1);
+                    message = args.join(" ");
+                } else if (args.length > 2) {
 
-                            let linkIndex;
-                            let argsValue;
-                            let cubeCraftLink = "https://www.cubecraft.net/members/"
-                            //check at what index the element has a substring of cubeCraftLink
-                            for (linkIndex = 1; linkIndex < args.length; ++linkIndex){
-                                argsValue = args[linkIndex];
-                                if (argsValue.substring(0,cubeCraftLink.length) === cubeCraftLink) {
-                                    badUsage = false;
-                                    break; //stop when you've got the index of the link
-                                } else {
-                                    badUsage = true;
-                                }
-                            }
+                    IGN = args[0]; //first element is IGN
+                    args = args.splice(1); //remove IGN from array
 
-                            if (badUsage){
-                                say(channelID, "Make sure to include the link to your forums profile!");
+                    let linkIndex;
+                    let argsValue;
+                    let cubeCraftLink = "https://www.cubecraft.net/members/"
+                    //check at what index the element has a substring of cubeCraftLink
+                    for (linkIndex = 0; linkIndex < args.length; ++linkIndex){
+                        argsValue = args[linkIndex];
+
+                        if (argsValue.substring(0,cubeCraftLink.length) === cubeCraftLink) {
+                            if (linkIndex === 0){
+                                say(channelID, "Did you include your username and the game-mode?");
                                 return;
                             }
-
-                            let forumLink = args[linkIndex];
-
-                            let GM = args.splice(0, linkIndex); //Get all the words upto the forum link and remove them from args
-                            GM = GM.join(" ");
-                            args = args.splice(1); //remove forums link from args
-                            message = args.join(" "); //join the rest of args as the message
-                            bot.sendMessage({
-                                to: bind,
-                                message: '',
-                                embed: {
-                                    color: 0xfecc52,
-                                    title: `New submission by ${evt.d.author.username}`,
-                                    fields: [{
-                                        name: "Details",
-                                        value: `IGN: **${IGN}**\n` +
-                                            `Game-mode: **${GM}**\n` +
-                                            `[${evt.d.author.username}'s forums profile](${forumLink})\n\n`
-
-                                    },
-                                        {
-                                            name: "Message:",
-                                            value: message +
-                                                `\n\n[**Click here to view their submission**](https://discord.com/channels/${evt.d.guild_id}/${evt.d.channel_id}/${evt.d.id})`
-                                        }
-                                    ],
-                                    timestamp: new Date(),
-                                    footer: {
-                                        text: "Don't forget to delete this message after reviewing!"
-                                    }
-                                }
-                            });
-                            say(channelID, `Your submission will be reviewed! <@${userID}>`);
+                            badUsage = false;
+                            break; //stop when you've got the index of the link
                         } else {
-                            say(channelID, "Not enough arguments. Use `$help` to see proper usage.");
+                            badUsage = true;
                         }
-                    });
+                    }
+
+                    if (badUsage){
+                        say(channelID, "Make sure to include the link to your forums profile!");
+                        return;
+                    }
+
+                    forumLink = args[linkIndex];
+
+                    GM = args.splice(0, linkIndex); //Get all the words upto the forum link and remove them from args
+                    GM = GM.join(" ");
+                    args = args.splice(1); //remove forums link from args
+                    message = args.join(" "); //join the rest of args as the message
+
+                    say(channelID, `Your submission will be reviewed! <@${userID}>`);
                 } else {
-                    say(channelID, "Error: I am not bound to any channel yet! use $bind to define the output channel.")
+                    say(channelID, "Not enough arguments. Use `$help` to see proper usage.");
+                    return;
                 }
+
+                bot.sendMessage({
+                    to: bind,
+                    message: '',
+                    embed: {
+                        color: 0xfecc52,
+                        title: `New submission by ${evt.d.author.username}`,
+                        fields: [{
+                            name: "Details",
+                            value: `IGN: **${IGN}**\n` +
+                                `Game-mode: **${GM}**\n` +
+                                `[${evt.d.author.username}'s forums profile](${forumLink})\n\n`
+
+                        },
+                            {
+                                name: "Message:",
+                                value: message +
+                                    `\n\n[**Click here to view their submission**](https://discord.com/channels/${evt.d.guild_id}/${evt.d.channel_id}/${evt.d.id})`
+                            }
+                        ],
+                        timestamp: new Date(),
+                        footer: {
+                            text: "Don't forget to delete this message after reviewing!"
+                        }
+                    }
+                });
+                /*else {
+                    say(channelID, "Error: I am not bound to any channel yet! use $bind to define the output channel.")
+                }*/
                 break;
             case "bind": //save a file with the channelID
                 let roleArr = evt.d.member.roles; //check what roles the user has
@@ -140,6 +162,16 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     });
                 }
                 break;
+            case "form":
+                bot.sendMessage({
+                    to: channelID,
+                    message: '',
+                    embed: {
+                        title: "CCG Records Form",
+                        url: "https://docs.google.com/forms/d/e/1FAIpQLSeSpzL1aHLTHUSmqNmMCuzlcvxVLiXzs8M_knuzy2ijuDuw4Q/viewform"
+                    }
+                })
+                break;
             case "link":
                 bot.sendMessage({
                     to: channelID,
@@ -149,6 +181,47 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                         url: "https://www.cubecraft.net/threads/cubecraft-book-of-world-records-revamping.213611/"
                     }
                 })
+                break;
+            case "connect":
+                if (args.length > 1){
+                    let linkIndex;
+                    let argsValue;
+                    let cubeCraftLink = "https://www.cubecraft.net/members/"
+                    let forumLink
+                    //check at what index the element has a substring of cubeCraftLink
+                    for (linkIndex = 0; linkIndex < args.length; ++linkIndex) {
+                        argsValue = args[linkIndex];
+
+                        if (argsValue.substring(0, cubeCraftLink.length) === cubeCraftLink) {
+                            if (linkIndex === 0) {
+                                say(channelID, "Type your name first and then the link :)");
+                                return;
+                            }
+                            forumLink = argsValue;
+                            args.splice(linkIndex, 1)
+                            break; //stop when you've got the index of the link
+                        }
+                    }
+
+                    let IGN = args.join(" ");
+
+                    if (!db.get('users').find({id: userID}).value()){
+                        db.get('users')
+                            .push({ id: userID, IGN: IGN, forums: forumLink})
+                            .write()
+                        say(channelID, "Successfully connected your account!")
+                    } else {
+                        db.get('users')
+                            .find({id:userID})
+                            .assign({IGN: IGN, forums: forumLink})
+                            .write()
+                        say(channelID, "Successfully updated your credentials.")
+                    }
+
+                }
+                else {
+                    say(channelID, "Please include your username and forum link.");
+                }
                 break;
             case "help":
                 bot.sendMessage({
@@ -171,6 +244,9 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                                 "Example of usage:\n" +
                                 "`$submit rubik_cube_man Parkour https://www.cubecraft.net/members/rubik_cube_man.5/ " +
                                 "\nFastest time Barn 1 5:232s [evidence]`\n\n" +
+
+                                "$form\n" +
+                                "- Sends a link to a google form through which you can submit your record too!\n\n" +
 
                                 "$link\n" +
                                 "- Sends a link to the CCG Records Thread."
